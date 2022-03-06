7161292f4c9c98f7a0cb0d786b5e7c448435b6e5
sachux <sachinkalpetta@gmail.com>
2022-03-06 23:31:27 +0530

    update mongo url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use('/students', studentRoutes);
 
-const CONNECTION_URL = "mongodb+srv://sachin:SachinJobs123@cluster0.33che.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://sachin:<<pwd>>@cluster0.33che.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000; 
 
