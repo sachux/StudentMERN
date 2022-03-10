3a109d31aeb7be9ed081c8c48804355b9f59ea9e
sachux <sachinkalpetta@gmail.com>
2022-03-10 22:54:29 +0530

    Update shows student with the student list

--- client/src/components/showStudent/showStudent.js
+++ client/src/components/showStudent/showStudent.js
@@ -45,26 +45,24 @@ useEffect(() => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Reg. Number</TableCell>
+            <TableCell align="right">Grade</TableCell>
+            <TableCell align="right">Section</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {studentList.map((student, key) => (
             <TableRow
-              key={row.name}
+              key={key}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {student.studentName}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{student.regNo}</TableCell>
+              <TableCell align="right">{student.grade}</TableCell>
+              <TableCell align="right">{student.section}</TableCell>
             </TableRow>
           ))}
         </TableBody>
